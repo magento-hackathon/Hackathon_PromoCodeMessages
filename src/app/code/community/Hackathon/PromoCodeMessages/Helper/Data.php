4808bf90cf175f5a1d1452eb18cee7065035dbbf
lfolco <me@laurafolco.com>
2014-09-21 13:29:59 -0400

    initial module creation

---
+++ src/app/code/community/Hackathon/PromoCodeMessages/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lfolco
+ * Date: 9/21/14
+ * Time: 1:19 PM
+ */ 
+class Hackathon_PromoCodeMessages_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
