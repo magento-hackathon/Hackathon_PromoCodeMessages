fa524252fc25e1d4fc229229f8636ccaaa682b75
lfolco <me@laurafolco.com>
2014-09-21 14:20:46 -0400

    added validator model; fleshed out observer

--- src/app/code/community/Hackathon/PromoCodeMessages/Model/Observer.php
+++ src/app/code/community/Hackathon/PromoCodeMessages/Model/Observer.php
@@ -1,18 +1,34 @@
 <?php
 
+
 class Hackathon_PromoCodeMessages_Model_Observer
 {
 
     /**
      * Called on sales_quote_collect_totals_after. Ensure that the action is couponPost before continuing.
+     *
      * @param Varien_Event_Observer $observer
      */
     public function validateCode(Varien_Event_Observer $observer)
     {
         $action = Mage::app()->getRequest()->getActionName();
-        if ($action == 'couponPost')
-        {
-            Mage::log('pepe');
+        if ($action == 'couponPost') {
+
+            if (Mage::app()->getRequest()->getParam('remove') == 1) {
+                return;
+            }
+
+            $quote = $observer->getQuote();
+            $couponCode = $quote->getCouponCode();
+
+            if (!$couponCode || $couponCode == '')
+            {
+                // parent validation has failed
+                $couponCode = (string)$this->getRequest()->getParam('coupon_code');
+                $msg = Mage::getModel('hackathon_promocodemessages/validator')->validate($couponCode);
+                return $msg;
+
+            }
         }
     }
 }
---
+++ src/app/code/community/Hackathon/PromoCodeMessages/Model/Validator.php
@@ -0,0 +1,10 @@
+<?php
+
+class Hackathon_PromoCodeMessages_Model_Validator extends Mage_Core_Model_Abstract
+{
+
+    public function validate($couponCode)
+    {
+        return "invalid";
+    }
+}
