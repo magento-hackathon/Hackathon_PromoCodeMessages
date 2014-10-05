4bbbad7f464497874145a9e8dacf2cec2bc633e6
Laura Folco <me@laurafolco.com>
2014-10-05 12:29:07 -0400

    added license notice

--- src/app/code/community/Hackathon/PromoCodeMessages/Model/Observer.php
+++ src/app/code/community/Hackathon/PromoCodeMessages/Model/Observer.php
@@ -1,5 +1,25 @@
 <?php
-
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension
+ * to newer versions in the future.
+ *
+ * @category   Hackathon
+ * @package    Hackathon_PromoCodeMessages
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 class Hackathon_PromoCodeMessages_Model_Observer
 {
